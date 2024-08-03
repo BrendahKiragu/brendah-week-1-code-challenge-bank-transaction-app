c63460a6f6547f5e7ee63c0edc86ff2baf2ef3e1
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-03 19:49:05 +0300

    creates the TransactionTable component

---
+++ src/components/TransactionTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function TransactionTable({ transactions, searchTerm }) {
+  // Filter transactions based on the searchTerm
+  const filteredTransactions = transactions.filter(transaction =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredTransactions.map((transaction, index) => (
+          <tr key={index}>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionTable;
