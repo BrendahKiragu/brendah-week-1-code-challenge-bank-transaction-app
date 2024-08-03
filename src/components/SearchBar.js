6f1b1e6527592c557d3423d6ccc3682869aebf56
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-03 19:52:10 +0300

    creates searchbar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function SearchBar({ onSearch }) {
+
+  //called when a user types a search term
+  //onSearch fn updates the searchterm state in the App component
+  const handleSearchChange = (e) => {
+    onSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      <label>Search:</label>
+      <input 
+        type="text" 
+        placeholder="Search transactions..." 
+        onChange={handleSearchChange} 
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
