2664baf9752ef3c2237ca4bfca880bd47ec73ae7
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-03 19:57:45 +0300

    links the children components to the App component

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React, { useState,useEffect } from 'react';
+import TransactionTable from './TransactionTable';
+import TransactionForm from './TransactionForm';
+import SearchBar from './SearchBar';
+import Header from './Header';
+
+function App() {
+
+  //creates 2 state variables: transactions and searchTerm
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetching  data from the local json-server
+    fetch('http://localhost:3000/transactions')
+      .then((response) => response.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.error('Error fetching transactions:', error));
+  }, []);
+
+  //adds a new transaction to our transactions array
+  const handleAddTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  //form the search input field
+  const handleSearchChange = (searchTerm) => {
+    setSearchTerm(searchTerm);
+  };
+
+    return (
+    <div>
+      <Header/> <hr></hr>
+      <SearchBar onSearch={handleSearchChange} />
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+      <TransactionTable transactions={transactions} searchTerm={searchTerm} />
+    </div>
+  );
+}
+
+export default App;
