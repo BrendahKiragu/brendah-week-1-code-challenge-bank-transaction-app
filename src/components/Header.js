f83d2a3c73a33a7e2e6fc77ca45e2879a6098a74
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-04 18:47:52 +0300

    conditional rendering of transaction details

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-function Header () {
+function Header ({onHandleLog,isLoggedin, accountDetails}) {
+
+  const showDetails = ()=>{
+    //conditional rendering of acc details based on log status
+    if(isLoggedin && accountDetails){
+      return (<div>
+        <p>AccountHolder : {accountDetails.accountHolder}</p>
+        <p>AccountHolder : {accountDetails.accountNumber}</p>
+      </div>)
+    }else {
+      return null
+    }
+  }
     return (
     <div>
       <h1>My Transactions Manager App</h1> 
+      <button className="button" onClick={onHandleLog}>{isLoggedin ? "LOGOUT" : "LOGIN"}</button>
+      {showDetails()}
     </div>
   )
 }
