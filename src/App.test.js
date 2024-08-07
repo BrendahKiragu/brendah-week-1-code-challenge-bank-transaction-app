69a9ab3910a7623d0598fa4f09f35cf1bcd8b024
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-07 12:00:00 +0300

    adds a test for checking the app name exists

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './components/App';
 
-test('renders learn react link', () => {
+test('My Transactions Manager App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("My Transactions Manager App");
   expect(linkElement).toBeInTheDocument();
 });
